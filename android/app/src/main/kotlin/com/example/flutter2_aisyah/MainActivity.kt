560821465d9734f76540ea6f6b5c0f644a797ba0
aisyaaulil <89905647+aisyaaulil@users.noreply.github.com>
2022-03-01 21:49:33 +0700

    02-flutter-aisyah

---
+++ android/app/src/main/kotlin/com/example/flutter2_aisyah/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter2_aisyah
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+/* 
+NAMA : AISYAH ULIL MAULINA
+KELAS: MI 2F
+NIM  : 2031710026
+ */
+
+import 'package:flutter/material.dart';
+// import 'package:flutter2_aisyah/build_in_layout/pro_child.dart';
+// import 'package:flutter2_aisyah/build_in_layout/pro_decor.dart';
+// import 'package:flutter2_aisyah/build_in_widget/text.dart';
+// import 'package:flutter2_aisyah/build_in_widget/image.dart';
+// import 'package:flutter2_aisyah/build_in_widget/cupertino.dart';
+// import 'package:flutter2_aisyah/build_in_widget/button.dart';
+// import 'package:flutter2_aisyah/build_in_widget/scafold.dart';
+// import 'package:flutter2_aisyah/build_in_widget/dialog.dart';
+// import 'package:flutter2_aisyah/build_in_widget/input.dart';
+// import 'package:flutter2_aisyah/build_in_widget/dateandtime.dart';
+// import 'package:flutter2_aisyah/build_in_layout/pro_child.dart';
+// import 'package:flutter2_aisyah/build_in_layout/pro_aligment.dart';
+// import 'package:flutter2_aisyah/build_in_layout/pro_color.dart';
+// import 'package:flutter2_aisyah/build_in_layout/pro_color.dart';
+// import 'package:flutter2_aisyah/build_in_layout/pro_height_weight.dart';
+// import 'package:flutter2_aisyah/build_in_layout/pro_margin.dart';
+// import 'package:flutter2_aisyah/build_in_layout/pro_padding.dart';
+// import 'package:flutter2_aisyah/build_in_layout/pro_transform.dart';
+// import 'package:flutter2_aisyah/build_in_layout/pro_decor.dart';
+// import 'package:flutter2_aisyah/build_in_layout/column_widget.dart';
+// import 'package:flutter2_aisyah/build_in_layout/row_widget.dart';
+// import 'package:flutter2_aisyah/build_in_layout/stack.dart';
+// import 'package:flutter2_aisyah/build_in_layout/list.dart';
+import 'package:flutter2_aisyah/build_in_layout/grid_view.dart';
+
+/* void main() {
+  runApp(const MyApp());
+} */
+
+/* void main() {
+  runApp(const ButtonWithContainer());
+} */
+
+/* void main() {
+  runApp(const BottomAlignSample());
+} */
+
+/* void main() {
+  runApp(const ColorContainerSample());
+} */
+
+/* void main() {
+  runApp(const ImageTransformSample());
+} */
+
+/* void main() {
+  runApp(const ColumnWidgetSample());
+} */
+
+/* void main() {
+  runApp(const RowWidgetSample());
+}
+ */
+/* void main() {
+  runApp(const StackWidgetSample());
+} */
+
+/* void main() {
+  runApp(const ListViewSample());
+} */
+
+void main() {
+  runApp(const GridViewSample());
+}
